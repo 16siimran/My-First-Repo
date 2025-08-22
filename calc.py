4420ac648fb8081101da478192e541acd477f005
16siimran <simipatel166@gmail.com>
2025-08-22 17:37:32 +0530

    Area of circle

--- calc.py
+++ calc.py
@@ -1,2 +1,14 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+import math
+
+print("Choose a shape to find the area:")
+print("1. Circle")
+print("2. Square")
+print("3. Rectangle")
+print("4. Triangle")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == '1':
+    radius = float(input("Enter radius of the circle: "))
+    area = math.pi * radius * radius
+    print("Area of Circle =", area)
